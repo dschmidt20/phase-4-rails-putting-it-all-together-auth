69b90c27c64cdcf18f6d71b46e9da236a124f419
daniel <d.schmidt20@yahoo.com>
2022-02-10 14:47:38 -0800

    commit tree 4eb0e071610579712ca6a8b2cdef326187dea426

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+before_action 
+    def create
+        user = User.create(user_params)
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+          render json: user
+        else
+          render json: { error: "Not authorized" }, status: :unauthorized
+        end
+      end
+
+    private
+
+    def user_params
+        params.permit(:username, :image_url, :bio, :password, :password_confirmation)
+    end
+
+
+end
